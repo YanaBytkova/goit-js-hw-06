2cb7e7a90105da8a2e9960fed0aae7564f5b38ad
YanaBytkova <nedavnyaya.yana@gmail.com>
2023-08-07 21:17:45 +0300

    Update task-01.js

---
+++ js/task-01.js
@@ -0,0 +1,22 @@
+const listWithId = document.querySelector('#categories');
+console.log(listWithId);
+const listWithClass = document.querySelectorAll('.item');
+console.log(listWithClass);
+
+    console.log("Number of categories:", listWithClass.length);
+// const menuItemsByTagName = document.querySelectorAll("li");
+// console.log(menuItemsByTagName);
+
+    // listWithId.forEach(function(number, index) {
+    //     console.log();
+    // });
+    
+
+    function showEveryCategories(listWithClass) {
+  
+        listWithClass.forEach(function (element) {
+            console.log("Category:", h2);
+            // console.log ("Elements:", this.ul.length)
+        });
+        return 
+      }
