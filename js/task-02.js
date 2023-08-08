a69af7d78cf4b5b58a5ebd949f75daaa64a01b39
YanaBytkova <nedavnyaya.yana@gmail.com>
2023-08-08 20:02:18 +0300

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,14 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+const allIngredients = document.querySelector('#ingredients');
+ingredients.forEach(function (value) {
+  const listIngredients = document.createElement("li");
+  listIngredients.textContent = value;
+  listIngredients.classList.add("item");
+  allIngredients.append(listIngredients);
+  console.log (listIngredients);
+
+});
+
+
